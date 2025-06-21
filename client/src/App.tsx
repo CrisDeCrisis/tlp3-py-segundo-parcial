a40aac2eef29a4e64cffde629e6a3d483df0acba
Cristian D. Gonzalez <cristiand.gonzalez094@gmail.com>
2025-06-21 19:31:35 -0300

    feat: :tada: Inicio Proyecto

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+export default function App() {
+  return (
+    <div className="relative w-full h-screen overflow-hidden">
+      <img
+        src="observatory-skyless.svg"
+        alt="Observatorio"
+        className="absolute inset-0 w-full h-full object-cover"
+      />
+      <div className="relative z-10">
+
+      </div>
+    </div>
+  );
+}
