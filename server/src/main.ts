173642e1452df11bc1d0e154e8933306db8b33a6
Cristian D. Gonzalez <cristiand.gonzalez094@gmail.com>
2025-06-23 21:43:25 -0300

    fix: :adhesive_bandage: Habilitar CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'; 
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,23 +9,35 @@ async function bootstrap() {
 
   const PORT = 3000;
 
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-  }));
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Predición de NEO')
-    .setDescription('API para predecir si un asteroide está cerca de la Tierra usando un modelo de IA')
+    .setDescription(
+      'API para predecir si un asteroide está cerca de la Tierra usando un modelo de IA',
+    )
     .setVersion('1.0')
     .build();
 
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(PORT, () => {
     logger.log(`Servidor corriendo en http://localhost:${PORT}`);
-    logger.log(`Documentación Swagger disponible en http://localhost:${PORT}/api`);
+    logger.log(
+      `Documentación Swagger disponible en http://localhost:${PORT}/api`,
+    );
   });
 }
 
